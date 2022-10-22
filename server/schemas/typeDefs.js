0fb687d4cad2c613b6b77ac40aff501bcfee3677
Tao Bian <biantao0519@hotmail.com>
2022-10-22 12:59:41 +1100

    update serverside typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -17,8 +17,8 @@ const typeDefs = gql`
         price: Int
         totalRooms: Int
         parkingSpace: Int
-        isShareBill: Boolean
-        withFurniture: Boolean
+        isShareBill: String
+        withFurniture: String
         description: String
         ownerEmail: String
         ownerContact: String
@@ -31,8 +31,10 @@ const typeDefs = gql`
     }
 
     type Query {
+        users: [User]
+        user(username: String!): User
         me: User
-        rooms: [Room]!
+        rooms(username: String): [Room]
         room(roomId: ID!): Room 
     }
 
@@ -41,9 +43,9 @@ const typeDefs = gql`
 
         addUser(username: String!, email: String!, password: String!): Auth
 
-        addRoom(image: String, location: String, price: Int, totalRooms: Int, parkingSpace: Int, isShareBill: Boolean, withFurniture: Boolean, description: String, ownerEmail: String, ownerContact: String, createdAt: String): Room
+        addRoom(image: String, location: String, price: Int, totalRooms: Int, parkingSpace: Int, isShareBill: String, withFurniture: String, description: String, ownerEmail: String, ownerContact: String): Room
 
-        updateRoom(roomId: ID!, image: String, price: Int, parkingSpace: Int, isShareBill: Boolean, withFurniture: Boolean, description: String, ownerContact: String): Room
+        updateRoom(roomId: ID!, image: String, price: Int, parkingSpace: Int, isShareBill: String, withFurniture: String, description: String, ownerContact: String): Room
 
         removeRoom(roomId: ID!): Room
     }
@@ -53,7 +55,7 @@ module.exports = typeDefs;
 
 
 /**
- * mutation AddRoom($image: String, $location: String, $price: Int, $totalRooms: Int, $parkingSpace: Int, $isShareBill: Boolean, $withFurniture: Boolean, $description: String, $ownerEmail: String, $ownerContact: Int, $createdAt: String) {
+ * mutation AddRoom($image: String, $location: String, $price: Int, $totalRooms: Int, $parkingSpace: Int, $isShareBill: String, $withFurniture: String, $description: String, $ownerEmail: String, $ownerContact: Int, $createdAt: String) {
   addRoom(image: $image, location: $location, price: $price, totalRooms: $totalRooms, parkingSpace: $parkingSpace, isShareBill: $isShareBill, withFurniture: $withFurniture, description: $description, ownerEmail: $ownerEmail, ownerContact: $ownerContact, createdAt: $createdAt) {
     _id
     image
@@ -91,7 +93,7 @@ module.exports = typeDefs;
 
 
 /**
- * mutation UpdateRoom($roomId: ID!, $image: String, $price: Int, $parkingSpace: Int, $isShareBill: Boolean, $withFurniture: Boolean, $description: String, $ownerContact: Int) {
+ * mutation UpdateRoom($roomId: ID!, $image: String, $price: Int, $parkingSpace: Int, $isShareBill: String, $withFurniture: String, $description: String, $ownerContact: Int) {
   updateRoom(roomId: $roomId, image: $image, price: $price, parkingSpace: $parkingSpace, isShareBill: $isShareBill, withFurniture: $withFurniture, description: $description, ownerContact: $ownerContact) {
     _id
     image
@@ -118,4 +120,54 @@ module.exports = typeDefs;
   "description": "efg",
   "ownerContact": 5555555
 }
+ */
+
+
+/** ADD USER ----- new backend test
+ * mutation Mutation($username: String!, $email: String!, $password: String!) {
+  addUser(username: $username, email: $email, password: $password) {
+    token
+  }
+}
+ * 
+ * {  "username": "testuser",
+  "email": "testuser@mail.com",
+  "password": "testuser"
+}
+ */
+
+/** LOGIN ----- new backend test
+ * ----Mutation
+ * mutation Login($email: String!, $password: String!) {
+  login(email: $email, password: $password) {
+    token
+    user {
+      _id
+      username
+      email
+      password
+    }
+  }
+}
+
+ -----Variables
+ * {  "email": "testuser@mail.com",
+  "password": "testuser"
+}
+ * 
+----- Out come
+{
+  "data": {
+    "login": {
+      "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7InVzZXJuYW1lIjoidGVzdHVzZXIiLCJlbWFpbCI6InRlc3R1c2VyQG1haWwuY29tIiwiX2lkIjoiNjM0OWQ2YTg1ZTA2ZDIwYzYwYzkxNjlkIn0sImlhdCI6MTY2NTc4MzkxMSwiZXhwIjoxNjY1NzkxMTExfQ.gZtkpAE4jhdQFX12Wl2SyuetK6evN6XoiPCZhMCSznw",
+      "user": {
+        "_id": "6349d6a85e06d20c60c9169d",
+        "username": "testuser",
+        "email": "testuser@mail.com",
+        "password": "$2b$10$GjtvEsDJexMH41j8slWCw.Hg1vIcZ6/8/f8Kr3Tr2Oht3o7xFc1Eu"
+      }
+    }
+  }
+}
+
  */
