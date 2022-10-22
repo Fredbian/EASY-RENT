2e9e0cf5e67a55873b14ce481e0843aaafbdf442
Tao Bian <biantao0519@hotmail.com>
2022-10-22 15:16:46 +1100

    update Navigation.js

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -1,83 +1,232 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { Box, Text } from "@chakra-ui/react"
-// import { Navbar, Nav, Container, Modal, Tab } from 'react-bootstrap';
-// import SignUpForm from './SignupForm';
-// import LoginForm from './LoginForm';
-//TODO: import other pages
-
+import {
+  Box,
+  Flex,
+  Text,
+  IconButton,
+  Button,
+  Stack,
+  Collapse,
+  Link,
+  useColorModeValue,
+  useDisclosure,
+} from '@chakra-ui/react';
+import {
+  HamburgerIcon,
+  CloseIcon,
+} from '@chakra-ui/icons';
 import Auth from '../utils/auth';
+import { Link as ReactLink } from 'react-router-dom';
 
-const AppNavbar = () => {
-    // set modal display state
-    const [showModal, setShowModal] = useState(false)
-
-    return (
-        <>
-          {/* <Navbar bg='dark' variant='dark' expand='lg'>
-            <Container fluid>
-              <Navbar.Brand as={Link} to='/'>
-                EASY RENT
-              </Navbar.Brand>
-              <Navbar.Toggle aria-controls='navbar' />
-              <Navbar.Collapse id='navbar'>
-                <Nav className='ml-auto'>
-                  <Nav.Link as={Link} to='/'>
-                    Home
-                  </Nav.Link>
-                  if user is logged in show Dashboard and logout
-                  {Auth.loggedIn() ? (
-                    <>
-                      <Nav.Link as={Link} to='/dashboard'>
-                        Dashboard
-                      </Nav.Link>
-                      <Nav.Link onClick={Auth.logout}>Logout</Nav.Link>
-                    </>
-                  ) : (
-                    <Nav.Link onClick={() => setShowModal(true)}>Login/Sign Up</Nav.Link>
-                  )}
-                </Nav>
-              </Navbar.Collapse>
-            </Container>
-          </Navbar>
-          set modal data up
-          <Modal
-            size='lg'
-            show={showModal}
-            onHide={() => setShowModal(false)}
-            aria-labelledby='signup-modal'>
-            tab container to do either signup or login component
-            <Tab.Container defaultActiveKey='login'>
-              <Modal.Header closeButton>
-                <Modal.Title id='signup-modal'>
-                  <Nav variant='pills'>
-                    <Nav.Item>
-                      <Nav.Link eventKey='login'>Login</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                      <Nav.Link eventKey='signup'>Sign Up</Nav.Link>
-                    </Nav.Item>
-                  </Nav>
-                </Modal.Title>
-              </Modal.Header>
-              <Modal.Body>
-                <Tab.Content>
-                  <Tab.Pane eventKey='login'>
-                    <LoginForm handleModalClose={() => setShowModal(false)} />
-                  </Tab.Pane>
-                  <Tab.Pane eventKey='signup'>
-                    <SignUpForm handleModalClose={() => setShowModal(false)} />
-                  </Tab.Pane>
-                </Tab.Content>
-              </Modal.Body>
-            </Tab.Container>
-          </Modal> */}
-        
+import { useQuery } from '@apollo/client'
+import { QUERY_USER, QUERY_ME } from '../utils/queries'
+import { useParams } from 'react-router-dom';
 
 
+export default function Navigation() {
+  const { username: userParam } = useParams();
 
-        </>
-      );
+    const { loading, data, refetch } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
+        variables: { username: userParam },
+    });
+
+    refetch()
+
+  const { isOpen, onToggle } = useDisclosure();
+
+  const logout = (event) => {
+    event.preventDefault();
+    Auth.logout();
+  };
+
+  if (loading) {
+    return <div>Loading...</div>;
 }
 
-export default AppNavbar
+  return (
+    <Box>
+      <Flex
+        bg={'white'}
+        color={'white'}
+        minH={'60px'}
+        py={{ base: 2 }}
+        px={{ base: 4 }}
+        borderBottom={1}
+        borderStyle={'solid'}
+        borderColor={'gray.200'}
+        align={'center'}>
+        <Flex
+          flex={{ base: 1, md: 'auto' }}
+          ml={{ base: -2 }}
+          display={{ base: 'flex', md: 'none' }}>
+          <IconButton
+            onClick={onToggle}
+            icon={
+              isOpen ? <CloseIcon w={3} h={3} /> : <HamburgerIcon w={5} h={5} />
+            }
+            variant={'ghost'}
+            aria-label={'Toggle Navigation'}
+          />
+        </Flex>
+        <Flex flex={{ base: 1 }} justify={{ base: 'center', md: 'start' }}>
+          <Text
+            fontSize="lg" fontWeight="bold"
+            textAlign={{ base: 'center', md: 'left' }}
+            fontFamily={'heading'}
+            color={'gray.800'}>
+            EASY RENT
+          </Text>
+
+          <Flex display={{ base: 'none', md: 'flex' }} ml={10}>
+            <DesktopNav />
+          </Flex>
+        </Flex>
+
+        <Stack
+          flex={{ base: 1, md: 0 }}
+          justify={'flex-end'}
+          direction={'row'}
+          spacing={6}>
+          {Auth.loggedIn() ? (
+            <>
+              <Button
+                // display={{ base: 'none', md: 'inline-flex' }}
+                fontSize={'sm'}
+                fontWeight={600}
+                onClick={logout}
+                color={'white'}
+                bg={'pink.400'}
+                _hover={{
+                  bg: 'pink.300',
+                }}>
+                Logout
+              </Button>
+            </>
+          ) : (
+            <>
+              <Button
+                as={ReactLink}
+                fontSize={'sm'}
+                fontWeight={400}
+                variant={'link'}
+                to={'/login'}>
+                Login
+              </Button>
+              <Button
+                as={ReactLink}
+                display={{ base: 'none', md: 'inline-flex' }}
+                fontSize={'sm'}
+                fontWeight={600}
+                color={'white'}
+                bg={'pink.400'}
+                to={'/signup'}
+                _hover={{
+                  bg: 'pink.300',
+                }}>
+                Sign Up
+              </Button>
+            </>
+          )}
+        </Stack>
+      </Flex>
+
+      <Collapse in={isOpen} animateOpacity>
+        <MobileNav />
+      </Collapse>
+    </Box>
+  );
+}
+
+const DesktopNav = () => {
+  const linkColor = useColorModeValue('gray.600', 'gray.200');
+  const linkHoverColor = useColorModeValue('gray.800', 'white');
+
+  return (
+    <Stack direction={'row'} spacing={4}>
+      <Box>
+        <Link
+          p={2}
+          as={ReactLink}
+          to='/'
+          fontSize={'sm'}
+          fontWeight={500}
+          color={linkColor}
+          _hover={{ textDecoration: 'none', color: linkHoverColor, }}
+        >
+          Home
+        </Link>
+
+        {Auth.loggedIn() ? (
+          <>
+            <Link p={2}
+              to='/dashboard'
+              as={ReactLink}
+              fontSize={'sm'}
+              fontWeight={500}
+              color={'pink.300'}
+              _hover={{ textDecoration: 'none', color: linkHoverColor, }}>
+              {Auth.getProfile().data.username}'s Dashboard
+            </Link>
+          </>
+        ) : (
+          <>
+
+          </>
+        )}
+      </Box>
+    </Stack>
+  );
+};
+
+
+const MobileNav = () => {
+  return (
+    <Stack
+      bg={useColorModeValue('white', 'gray.800')}
+      p={4}
+      spacing={4}
+      display={{ md: 'none' }}>
+      <Flex
+        py={2}
+        as={ReactLink}
+        to='/'
+        justify={'space-between'}
+        align={'center'}
+        _hover={{
+          textDecoration: 'none',
+        }}>
+        <Text
+          fontWeight={600}
+          color={useColorModeValue('gray.600', 'gray.200')}>
+          Home
+        </Text>
+      </Flex>
+
+      {Auth.loggedIn() ? (
+        <>
+          <Flex
+            py={2}
+            as={ReactLink}
+            to='/dashboard'
+            justify={'space-between'}
+            align={'center'}
+            _hover={{
+              textDecoration: 'none',
+            }}>
+            <Text
+              fontWeight={600}
+              color={'pink.300'}
+              >
+              {Auth.getProfile().data.username}'s Dashboard
+            </Text>
+          </Flex>
+        </>
+      ) : (
+        <>
+          
+        </>
+      )}
+
+    </Stack>
+  );
+};
+
