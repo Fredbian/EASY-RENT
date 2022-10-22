94a7767613e9aa9083e1b74be00c443b35d58e81
Tao Bian <biantao0519@hotmail.com>
2022-10-22 12:49:12 +1100

    update login page

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,39 +1,160 @@
-import React from "react";
+import React, { useState } from "react";
 import {
-  Tabs,
-  TabList,
-  Tab,
-  TabPanel,
-  TabPanels,
+  Flex,
   Box,
-  useColorModeValue,
-} from "@chakra-ui/core";
-import LoginForm from "../components/LoginForm";
-import SignupForm from "../components/SignupForm";
+  FormControl,
+  FormLabel,
+  Input,
+  Stack,
+  Link,
+  Button,
+  Heading,
+  Text,
+} from '@chakra-ui/react';
 
-// import chakraUILight from "../assets/images/chakra-ui-light.png";
-// import chakraUIDark from "../assets/images/chakra-ui-dark.png";
+// import useMutation and LOGIN-USER
+import { useMutation } from "@apollo/react-hooks";
+import { LOGIN_USER } from "../utils/mutations";
+// import { validateEmail } from '../utils/helpers'
+import Auth from "../utils/auth";
+import { Link as ReactLink } from 'react-router-dom';
+import { QUERY_ME } from "../utils/queries";
 
-export default function Form() {
-  const bgColor = useColorModeValue("gray.200", "gray.700");
-//   const chakraUI = useColorModeValue(chakraUILight, chakraUIDark);
+// redirect -----
+import { useNavigate } from "react-router-dom";
+
+
+const errorStyle = {
+  fontWeight: '700',
+  color: 'red'
+}
+
+export default function LoginForm() {
+  const [userFormData, setUserFormData] = useState({ email: "", password: "" });
+
+  const [loginUser, { error, data }] = useMutation(LOGIN_USER, {
+    refetchQueries: [
+      { query: QUERY_ME },
+    ]
+  });
+
+  // redirect -----
+  const navigate = useNavigate()
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setUserFormData({ ...userFormData, [name]: value });
+  }
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+
+    // use loginUser function
+    try {
+      const { data } = await loginUser({
+        variables: { ...userFormData },
+      });
+
+      Auth.login(data.login.token);
+    } catch (e) {
+      console.error(e);
+    }
+
+    setUserFormData({
+      // username: "",
+      email: "",
+      password: "",
+    });
+
+    //redirect ----
+    const path = '/dashboard'
+    navigate(path)
+  };
+
+
   return (
-    <Box bgColor={bgColor} p={3} w="100%" boxShadow="lg" borderRadius="lg">
-      <h2 className="title">LogIn & SignUp</h2>
-      <Tabs isFitted>
-        <TabList>
-          <Tab _focus={{ boxShadow: "none" }}>Log In</Tab>
-          <Tab _focus={{ boxShadow: "none" }}>Sign Up</Tab>
-        </TabList>
-        <TabPanels>
-          <TabPanel>
-            <LoginForm />
-          </TabPanel>
-          <TabPanel>
-            <SignupForm />
-          </TabPanel>
-        </TabPanels>
-      </Tabs>
-    </Box>
+    <>
+      {data ? (
+        <p>
+          Success! You may now head{' '}
+          <Link as={ReactLink} to="/me">to Dashboard.</Link>
+        </p>
+      ) : (
+        <form onSubmit={handleFormSubmit} id='loginForm'>
+          <Flex
+            minH={'100vh'}
+            align={'center'}
+            justify={'center'}
+            bg={'gray.50'}>
+            <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
+              <Stack align={'center'}>
+                <Heading fontSize={'4xl'}>Login to your account</Heading>
+                <Text fontSize={'lg'} color={'gray.600'}>
+                  to post your room infomation ✌️
+                </Text>
+              </Stack>
+              <Box
+                rounded={'lg'}
+                bg={'white'}
+                boxShadow={'lg'}
+                p={8}>
+                <Stack spacing={4}>
+                  <FormControl id="email" isRequired>
+                    <FormLabel>Email address</FormLabel>
+                    <Input
+                      type="email"
+                      name='email'
+                      id='email'
+                      value={userFormData.email}
+                      onChange={handleInputChange}
+                    />
+                  </FormControl>
+                  <FormControl id="password" isRequired>
+                    <FormLabel>Password</FormLabel>
+                    <Input
+                      type="password"
+                      name='password'
+                      id='password'
+                      value={userFormData.password}
+                      onChange={handleInputChange}
+                    />
+                  </FormControl>
+                  <Stack spacing={10}>
+                    <Stack
+                      direction={{ base: 'column', sm: 'row' }}
+                      align={'start'}
+                      justify={'space-between'}>
+                      <Text fontSize={'lg'} color={'gray.600'}>
+                        Don't have an account?
+                      </Text>
+                      <Link as={ReactLink} to="/signup" color={'blue.400'}>Sign Up</Link>
+                    </Stack>
+                    {error && <div style={errorStyle}>Login failed!</div>}
+                    <Button
+                      bg={'blue.400'}
+                      color={'white'}
+                      _hover={{
+                        bg: 'blue.500',
+                      }}
+                      disabled={
+                        !(
+                          userFormData.email &&
+                          userFormData.password
+                        )
+                      }
+                      type='submit'
+                      form="loginForm"
+                    >
+                      Login
+                    </Button>
+                  </Stack>
+                </Stack>
+              </Box>
+            </Stack>
+          </Flex>
+        </form>
+
+      )}
+    </>
   );
 }
